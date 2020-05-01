03fddd562462083721dd3e8101dc0d76fd64549e
adamkearn <adamkearn17@gmail.com>
2020-05-02 00:28:54 +0100

    Cleaned up code a bit
    Just tidied up my code a little bit.   :)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const timestamp = require('time-stamp');
 
-function sendWebhook (embeds) {
+function sendWebhook(embeds) {
   const options = {
     uri: 'https://discordapp.com/api/webhooks/702281289855795241/YrV2uqC2Xf65oHCHL-35D7HhpxdWxY1_q2HCdh0RhPS7DerWTKPBus-0_nonRvKr58wg',
     method: 'POST',
@@ -14,6 +14,7 @@ function sendWebhook (embeds) {
   };
 
   request(options, function(err, response, body) {
+    console.log('Sending POST request...')
     if (err) {
       console.log(err);
       return;
@@ -21,11 +22,11 @@ function sendWebhook (embeds) {
   });
 }
 
-function onAuthenticationFailed (username) {
-  const embeds = {
-    "title": "Authentication Failed",
-    "description": `Scripter-X detected that Authentication has failed with the following user:\n**${username}**`,
-    "color": 15746887,
+function generateEmbed(title, description, color) {
+  const embed = {
+    "title": title,
+    "description": description,
+    "color": color,
     "footer": {
       "text": "From",
       "icon_url": "https://images-eu.ssl-images-amazon.com/images/I/51WJTxymC6L.png"
@@ -33,7 +34,18 @@ function onAuthenticationFailed (username) {
     "timestamp": `${timestamp('YYYY-MM-DDTHH:mm:ss')}`
   }
 
-  sendWebhook(embeds);
+  return embed
+}
+
+function onAuthenticationFailed(username) {
+  console.log('EVENT: onAuthenticationFailed was triggered.');
+  const result = generateEmbed(
+    "Authentication Failed",
+    `Scripter-X detected that Authentication has failed with the following user:\n**${username}**`,
+    15746887
+  );
+
+  sendWebhook(result);
 }
 
 // Send a test Webhook.
