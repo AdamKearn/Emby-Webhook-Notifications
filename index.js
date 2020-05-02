eaeec3d5f0a9643217c05517232e0e5ac74e102a
adamkearn <adamkearn17@gmail.com>
2020-05-02 02:07:51 +0100

    Added yargs support
    Added the yargs package to handle all the arguments.

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 const request = require("request");
 const timestamp = require('time-stamp');
+const yargs = require('yargs');
+
+const argv = yargs
+    .command('onAuthenticationFailed', 'When incorrect login details are provided this event is called.')
+    .option('ping', {
+        alias: 'p',
+        description: 'Pings a user/role. MUST include the @ symbol.',
+        type: 'string'
+    })
+    .option('webhook', {
+        alias: 'w',
+        description: 'The Discord Webhook URL',
+        type: 'string'
+    })
+    .option('data', {
+        alias: 'd',
+        description: 'Pass data into the functions as an array.',
+        type: 'array'
+    })
+    .help()
+    .alias('help', 'h')
+    .demandOption(['webhook', 'data'], 'The webhook and data arguments are needed.')
+    .argv;
+
+if (!argv.webhook) {
+  console.log('the --webhook option must have the discord webhook URL.');
+  process.exit();
+}
+
+if (argv.data === undefined || argv.data == 0) {
+  console.log('You must pass a value into --data');
+  process.exit();
+}
+
+if (argv._.includes('onAuthenticationFailed')) {
+  onAuthenticationFailed('asd');
+}
+
+function getMentions() {
+  if (argv.ping) {
+    return argv.ping
+  }
+}
 
 function sendWebhook(embeds) {
   const options = {
-    uri: 'https://discordapp.com/api/webhooks/702281289855795241/YrV2uqC2Xf65oHCHL-35D7HhpxdWxY1_q2HCdh0RhPS7DerWTKPBus-0_nonRvKr58wg',
+    uri: argv.webhook,
     method: 'POST',
     json: {
       "username": "Emby",
       "avatar_url": "https://images-eu.ssl-images-amazon.com/images/I/51WJTxymC6L.png",
-      "content": "@everyone",
+      "content": getMentions(),
       "embeds": [embeds]
     }
   };
@@ -41,12 +84,9 @@ function onAuthenticationFailed(username) {
   console.log('EVENT: onAuthenticationFailed was triggered.');
   const result = generateEmbed(
     "Authentication Failed",
-    `Scripter-X detected that Authentication has failed with the following user:\n**${username}**`,
+    `Scripter-X detected that Authentication has failed with the following user:\n**${argv.data[0]}**`,
     15746887
   );
 
   sendWebhook(result);
 }
-
-// Send a test Webhook.
-onAuthenticationFailed("test")
